23d2b8cdb414c1605901c1dd74f9066d76b381c2
Michael Shuff <mjshuff23@gmail.com>
2021-02-25 19:51:43 -0500

    removed tic-tac-toe from this issue

--- tic-tac-toe-solution/index.js
+++
@@ -1,9 +0,0 @@
-const TicTacToeGame = require('./game');
-const ComputerPlayer = require('./computerPlayer');
-const HumanPlayer = require('./humanPlayer');
-
-const player1 = new HumanPlayer();
-const player2 = new HumanPlayer('o');
-const player3 = new ComputerPlayer();
-const game = new TicTacToeGame(player1, player2);
-game.start();
